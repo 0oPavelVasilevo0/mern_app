2fd0483940ce203cb3926648ac8bed3fd2510eb5
Pavel Vasilev <karterpahan@gmail.com>
2023-12-15 13:43:23 +0100

    fix

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -19,7 +19,7 @@ const userSchema = new Schema({
 // static signup method
 userSchema.statics.signup = async function (email, password) {
 
-    //validation
+    // validation
     if (!email || !password) {
         throw Error('All fields must be filled')
     }
@@ -41,25 +41,6 @@ userSchema.statics.signup = async function (email, password) {
 
     const user = await this.create({ email, password: hash })
 
-    return user
-}
-
-//static login method
-userSchema.statics.login = async function(email, password) {
-    if (!email || !password) {
-        throw Error('All fields must be filled')
-    }
-    const user = await this.findOne({ email })
-
-    if (!user) {
-        throw Error('Incorrect email')
-    }
-
-    const match = await bcrypt.compare(password, user.password) 
-
-    if(!match) {
-        throw Error('Incorrect password')
-    }
     return user
 }
 
