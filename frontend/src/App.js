ce47c5dd0d8ed7816e7a044bdfc427da41b0c61f
Pavel Vasilev <karterpahan@gmail.com>
2023-12-19 11:23:31 +0100

    React Routes fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 // pages & components
 
@@ -7,8 +7,12 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
       <Router>
@@ -17,15 +21,15 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={<Home />}
+              element={user ? <Home /> : <Navigate to="/login" />}
             />
             <Route
-            path='/login'
-            element={<Login />}
+              path='/login'
+              element={!user ? <Login /> : <Navigate to="/" />}
             />
             <Route
               path='/signup'
-              element={<Signup />}
+              element={!user ? <Signup /> : <Navigate to="/" />}
             />
           </Routes>
         </div>
