dc148b856f50dbdaec2a74aa78f7e580f16abc90
Pavel Vasilev <karterpahan@gmail.com>
2024-01-03 17:44:42 +0100

    fix

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,10 @@ app.use('/api/user', userRoutes)
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         // listen for requests
-        app.listen(process.env.PORT, () => {
+        // app.listen(process.env.PORT, () => {
+        //     console.log('connected to db & listening on port', process.env.PORT)
+        // })
+        app.listen(4000, () => {
             console.log('connected to db & listening on port', process.env.PORT)
         })
     })
