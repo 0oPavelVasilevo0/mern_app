7504648d1953b7302206717115ef38e791388091
Pavel Vasilev <karterpahan@gmail.com>
2024-01-18 14:57:53 +0100

    create work .env&postdata,fetchData in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 // pages & components
@@ -12,6 +12,39 @@ import { useAuthContext } from './hooks/useAuthContext';
 function App() {
 
   const { user } = useAuthContext()
+
+  const apiUrl = process.env.REACT_APP_BACKEND_URL;
+
+  useEffect(() => {
+    fetchData();
+    postData({ someData: 'example' });
+  },);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${apiUrl}/api/data`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const postData = async (bodyData) => {
+    try {
+      const response = await fetch(`${apiUrl}/api/data`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bodyData),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error posting data:', error);
+    }
+  };
 
   return (
     <div className="App">
